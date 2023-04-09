f37aa0d6ead8cdba6434c2f370c04c86fb704d60
Sheriff <realexcel2020@gmail.com>
2023-04-09 23:49:56 +0100

    added the terraform api endpoint

--- script.js
+++ script.js
@@ -10,24 +10,36 @@ function updateCount(){
     
 }
 
+// function addCount(){
+//     fetch("https://jwow2heow8.execute-api.us-east-1.amazonaws.com/dev", {
+//         method: "POST",
+//         body: JSON.stringify(setData),
+//         headers: {
+//             "Content-type": "application/json; charset=UTF-8"
+//           },
+//           mode: "no-cors"  
+//     })
+//     .then(res => console.log(res))
+//     fetch("https://a3eyxf14g7.execute-api.us-east-1.amazonaws.com/default/readFromTable")
+//         .then((res) => {
+//             res.json().then(data => {
+//                 count = data[0].view_Count
+//                 counthtml.innerText = count
+//             })
+//         })
+//     // .then(json => console.log(json))
+// }
+
 function addCount(){
-    fetch("https://jwow2heow8.execute-api.us-east-1.amazonaws.com/dev", {
-        method: "POST",
+    fetch("https://6ihtiyek0k.execute-api.us-east-1.amazonaws.com/dev/views", {
+        method : "POST",
         body: JSON.stringify(setData),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-          },
-          mode: "no-cors"  
     })
-    .then(res => console.log(res))
-    fetch("https://a3eyxf14g7.execute-api.us-east-1.amazonaws.com/default/readFromTable")
-        .then((res) => {
-            res.json().then(data => {
-                count = data[0].view_Count
-                counthtml.innerText = count
-            })
-        })
-    // .then(json => console.log(json))
+    .then(response => response.json())
+    .then(data => {
+        count = data
+     counthtml.innerText = count
+    })
 }
 
 addCount()
